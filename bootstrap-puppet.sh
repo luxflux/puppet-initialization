86c0be6022923c85cdeec69d0625ae88f6878c1e
Raffael Schmid <raf@nine.ch>
2014-08-07 12:55:38 +0200

    download the keys automatically

--- bootstrap-puppet.sh
+++ bootstrap-puppet.sh
@@ -2,17 +2,14 @@
 
 set -e
 
+authorized_keys_url='https://raw.githubusercontent.com/luxflux/puppet-initialization/master/authorized_keys'
 password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
-
-echo "Please give me your public ssh key: "
-read ssh_key
-echo ""
 
 set -x
 useradd -m -g admin -s /bin/bash admin
 echo "admin:${password}" | sudo chpasswd ;
 
 mkdir -p -m 700 ~admin/.ssh
-echo ${ssh_key} > ~admin/.ssh/authorized_keys
+wget -O ~admin/.ssh/authorized_keys ${authorized_keys_url}
 chown -R admin. ~admin/.ssh
 chmod 600 ~admin/.ssh/authorized_keys
