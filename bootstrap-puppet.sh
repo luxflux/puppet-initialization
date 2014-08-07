e7e01a6f821f91335f88800acb2360bea1937dda
Raffael Schmid <raf@nine.ch>
2014-08-07 13:37:24 +0200

    add sudoers entry

--- bootstrap-puppet.sh
+++ bootstrap-puppet.sh
@@ -7,9 +7,11 @@ password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
 
 set -x
 useradd -m -s /bin/bash admin
-echo "admin:${password}" | sudo chpasswd ;
+echo "admin:${password}" | sudo chpasswd
 
 mkdir -p -m 700 ~admin/.ssh
 wget -O ~admin/.ssh/authorized_keys ${authorized_keys_url}
 chown -R admin. ~admin/.ssh
 chmod 600 ~admin/.ssh/authorized_keys
+
+echo 'admin ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/admin
